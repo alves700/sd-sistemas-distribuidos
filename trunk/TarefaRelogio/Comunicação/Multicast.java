bc6d83329db2b6b907a316932687584d7a7a1747
vinicius_zaramella@hotmail.com <vinicius_zaramella@hotmail.com@6702cc2d-9b71-1591-90f3-58483b9a5d13>
2012-03-23 01:33:48 +0000

    Classe Multicast implementada.

--- trunk/TarefaRelogio/Comunicação/Multicast.java
+++ trunk/TarefaRelogio/Comunicação/Multicast.java
@@ -1,5 +1,87 @@
 package Comunicação;
 
-public class Multicast {
+import java.net.*;
+import java.util.ArrayList;
+import java.io.*;
 
+public class Multicast extends Thread{
+    
+    public MulticastSocket socket;
+    public InetAddress group;
+    public boolean isConnected;
+    private final String MCAdrress = "224.0.0.1";
+    public ArrayList<String> inBuffer;
+    
+    public Multicast(){
+    	inBuffer = new ArrayList<String>();
+    }
+    public void joinMulticast(){
+        socket =null;
+        try {
+            InetAddress group = InetAddress.getByName(MCAdrress);
+            socket = new MulticastSocket(6789);
+            socket.joinGroup(group);
+            this.isConnected = true;
+        }catch (SocketException e){System.out.println("Socket: " + e.getMessage());
+        }catch (IOException e){System.out.println("IO: " + e.getMessage());
+        }finally {if(socket != null) socket.close();}
+    }
+    public void leaveGroup() {
+        try {
+            this.isConnected = false;
+            socket.leaveGroup(group);
+            
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    public void enviaMsg(String msg){
+        byte [] m = msg.getBytes();
+        DatagramPacket messageOut = new DatagramPacket(m, m.length, group, 6789);
+        try {
+            socket.send(messageOut);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }    
+    }
+    public void recebeMsg(){
+            
+        byte[] buffer = new byte[1000];
+                // get messages from others in group
+                    DatagramPacket messageIn = new DatagramPacket(buffer, buffer.length);
+                    try {
+                        socket.receive(messageIn);
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+        inBuffer.add(new String(messageIn.getData()));
+  
+    }
+    public String getMsg(){
+    	if ( existeMsg() ){
+	    	String msg = inBuffer.get(0);
+	    	inBuffer.remove(0);
+	    	return msg;
+    	}
+    	else
+    		return null;
+    }
+    public boolean existeMsg(){
+    	if ( inBuffer.size() == 0)
+    		return false;
+    	else 
+    		return true;
+    }
+    @Override
+    public void run(){
+        while(true){
+            if ( isConnected){
+                recebeMsg();
+            }
+        }
+    }
 }
+
